f0e093ac0a16b162db0ac4d473ccb38a4eaa7bcf
jgh <github@inled.es>
2025-11-25 07:50:20 +0100

    update hero and news

--- src/data/news.ts
+++ src/data/news.ts
@@ -7,14 +7,14 @@ export interface NewsItem {
 }
 
 export const news: NewsItem[] = [
-    {
+    /*{
         title: "Recogida de propuestas",
         image: "./public/delegados.png",
         description: "Hemos creado una encuesta anónima para recoger propuestas que libremente deseeis hacer.",
         link: "https://live.e-survey.io/switch/share/2859b07c28",
         date: "24 Nov 2025"
     },
-   /* {
+    {
         title: "Recogida de propuestas",
         image: "https://images.unsplash.com/photo-1504711434969-e33886168f5c?q=80&w=2070&auto=format&fit=crop",
         description: "Hemos creado una encuesta anónima para recoger propuestas que libremente deseeis hacer.",
