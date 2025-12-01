5ec54d33e427818eee69889d37733f71f7196f73
jgh <github@inled.es>
2025-12-01 07:48:02 +0100

    update eventos + newscard problems

--- src/data/news.ts
+++ src/data/news.ts
@@ -9,16 +9,9 @@ export interface NewsItem {
 export const news: NewsItem[] = [
     {
         title: "Encuesta abierta",
-        image: "./public/delegados.png",
+        image: "delegados.png",
         description: "Hemos creado una encuesta anónima para recoger propuestas que libremente deseeis hacer.",
         link: "https://live.e-survey.io/switch/share/2859b07c28",
         date: "25 Nov 2025"
     },
-    {
-        title: "Charla excursión",
-        image: "https://images.unsplash.com/photo-1504711434969-e33886168f5c?q=80&w=2070&auto=format&fit=crop",
-        description: "El miércoles en el segundo recreo.",
-        link: "",
-        date: "25 Nov 2025"
-    }
 ];
