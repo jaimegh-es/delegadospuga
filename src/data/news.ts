d5eb108de31614dbefdf14819c865c604de7f13d
jgh <github@inled.es>
2025-12-12 15:25:23 +0100

    vamos a continuar

--- src/data/news.ts
+++ src/data/news.ts
@@ -14,4 +14,12 @@ export const news: NewsItem[] = [
         link: "/noticias/161225",
         date: "10 Dic 2025"
     },
+
+   /* {
+        title: "¡IMPORTANTE INSCRIBIRSE! Salida día 16",
+        image: "unitour.png",
+        description: "Es importante inscribirse para la salida...",
+        link: "/noticias/161225",
+        date: "10 Dic 2025"
+    },*/
 ];
