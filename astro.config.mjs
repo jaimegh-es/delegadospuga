147138d6f3fcdf5dd7df50553a05e510baa1aea9
Development <dev@Mac.versumdata.com>
2026-01-11 20:16:11 +0100

    nuevo moodle notifier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    AstroPWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '1B Bach | Noticias y examenes',
+        short_name: '1B Bach',
+        description: 'Sitio de noticias y exámenes para 1B Bachillerato',
+        theme_color: '#2000ad',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,pdf}'],
+        maximumFileSizeToCacheInBytes: 15 * 1024 * 1024,
+      }
+    })
+  ]
+});
